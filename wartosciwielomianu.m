c68945bb9b3d06cd61d062aa2ed218843a774024
i2070p <metiv9@gmail.com>
2013-10-24 14:52:43 +0200

    Update wartosciwielomianu.m

--- wartosciwielomianu.m
+++ wartosciwielomianu.m
@@ -1,8 +1,7 @@
-function [mx]=wartosciwielomianu(w_x,X,Y);
-    dziedzina = (X(1,1)-1):0.1:(X(1,end)+1);
-    mx=zeros(1,length(dziedzina));
-    for i=1:length(dziedzina),
-        ix = dziedzina(i);
+function [mx]=wartosciwielomianu(w_x,X,Y,D);
+    mx=zeros(1,length(D));
+    for i=1:length(D),
+        ix = D(i);
         for j=1:length(w_x),
             mx(1,i)=mx(1,i)+w_x(j)*ix^(j-1);
         end
