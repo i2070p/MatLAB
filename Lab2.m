c55f06c1f7b3bfdbed98b90b63d48bb45ff537fa
i2070p <metiv9@gmail.com>
2013-10-24 14:18:12 +0200

    Update Lab2.m

--- Lab2.m
+++ Lab2.m
@@ -7,44 +7,13 @@ for n=1:i_p,
     ky(1,n)=input(strcat('Podaj y',int2str(n),': '));
 end
 
-v=zeros(i_p);
-for n=1:i_p,
-    for o=1:i_p,
-        v(n,o)=kx(1,n)^(o-1);
-    end
-end
-
-a=(inv(v)*ky')'
+a=interpolacja(kx,ky);
 
 i_w=length(a)
 
-wx=a;
+wxi=wartosciwielomianu(a,kx,ky);
 
-dziedzina = (kx(1,1)-1):0.1:(kx(1,end)+1);
-wxi=zeros(1,length(dziedzina));
-for i=1:length(dziedzina),
-    ix = dziedzina(i);
-    for j=1:i_w,
-        wxi(1,i)=wxi(1,i)+wx(j)*ix^(j-1);
-    end
-end
-
-
-str='w(x)=';
-
-if (i_w>=2)
-for i=i_w:-1:2,
-    wi=wx(1,i);
-    if (wi<0), str = strcat(str, '-', int2str(abs(wi)),'*x^',int2str(i-1));
-    else str = strcat(str,'+',int2str(wi),'*x^',int2str(i-1));
-    end;
-end
-end
-if (wx(1,1)<0),
-str = strcat(str,'-',int2str(abs(wx(1,1))));
-else str = strcat(str,'+',int2str(wx(1,1)));
-end
 hold on;
 
-plot(dziedzina, wxi, kx, ky, 'r x')
-title(string(str));
+plot(dziedzina, wxi, kx, ky, 'r o')
+title(string(etykieta(wx)));
